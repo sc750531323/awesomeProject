f560b520641a762bfc681c692696db4dc2137d21
sc750531323 <750531323@qq.com>
2022-01-18 23:52:28 +0800

    gogogo

--- first.go
+++ first.go
@@ -11,5 +11,12 @@ func main() {
 		s += temp + os.Args[i]
 		temp = " "
 	}
-	fmt.Printf("s is %s \n",s)
+	fmt.Printf("s1 is %s \n",s)
+	s = ""
+	//迭代方式2
+	for _,arg := range os.Args {
+		s += temp + arg
+		temp = " "
+	}
+	fmt.Printf("s2 is %s \n",s)
 }
